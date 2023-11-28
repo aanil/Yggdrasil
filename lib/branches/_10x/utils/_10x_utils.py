8cb1fbdfa523e183ae581988e3ebe3496ec62019
glrs <5999366+glrs@users.noreply.github.com>
2023-11-28 16:18:27 +0100

    Add compile_metadata() to assist w/ move to strategy pattern

--- lib/branches/_10x/utils/_10x_utils.py
+++ lib/branches/_10x/utils/_10x_utils.py
@@ -43,6 +43,54 @@
 import logging
 from lib.utils.config_loader import ConfigLoader
 
+
+def compile_metadata(metadata, sample_name, project_info):
+    """
+    Collect and transform metadata for a given sample.
+
+    Args:
+        metadata (dict): The data for the sample to collect metadata for.
+        sample_name (str, optional): The name or identifier of the sample.
+
+    Returns:
+        dict: A dictionary containing metadata for the specified sample.
+    """
+    try:
+        # Load the 10x configuration
+        config = ConfigLoader().load_config_path("lib/branches/_10x/10x_config.json")
+
+        # Initialize the dictionary to store arguments for the sample
+        args_dict = {
+            "project_name": project_info['project_name'],
+            "customer_name": metadata.get('customer_name', ''),
+            "scilife_name": metadata.get('scilife_name', ''),
+            "comma_separated_fastq_file_paths": "",
+            "ref_genome_path": config['species_mappings'][project_info['library_prep_option']][project_info['ref_genome']],
+            "cellranger_path": config['cellranger_path'][project_info['library_prep_option']],
+        }
+
+        # Process library_prep information for the sample
+        for prep_id, prep_info in metadata.get('library_prep', {}).items():
+            # Iterate over the available sequenced flowcells
+            for sequenced_fc in prep_info.get('sequenced_fc', []):
+                fastq_path = f"{config['seq_root_dir']}/{sequenced_fc}/Demultiplexing/{project_info['project_name']}/Sample_{sample_name}"
+                args_dict["comma_separated_fastq_file_paths"] += f"{fastq_path},"
+
+        # Remove trailing comma from the FASTQ paths
+        args_dict["comma_separated_fastq_file_paths"] = args_dict["comma_separated_fastq_file_paths"].rstrip(',')
+
+        # Check for missing values in the sample level metadata
+        if not all(args_dict.values()):
+            logging.warning(f"Missing values for sample {sample_name}. Skipping metadata collection.")
+            return None
+
+        return args_dict
+
+    except Exception as e:
+        logging.warning(f"Error occurred while collecting metadata for sample '{sample_name}': {e}", exc_info=True)
+        return None
+
+
 def collect_meta_per_sample(doc):
     """
     Collect metadata for each sample based on the provided document.
