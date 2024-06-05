f7a8a28b23eeb6b1bd6c617861aa2426eb5b4b5d
glrs <5999366+glrs@users.noreply.github.com>
2024-06-05 13:57:05 +0200

    Refactor ConfigLoader class to improve code readability and documentation

--- lib/utils/config_loader.py
+++ lib/utils/config_loader.py
@@ -1,12 +1,12 @@
 import json
 import types
-import appdirs
+
+# NOTE: To use custom_logger resolve circular import issue
 import logging
 
 from pathlib import Path
 
 from lib.utils.common import YggdrasilUtilities as Ygg
-
 
 class ConfigLoader:
     """
@@ -16,24 +16,23 @@ class ConfigLoader:
     immutable view of the configuration data using MappingProxyType. It allows modules
     to access configuration settings without the ability to modify them.
 
-    Args:
-        file_name (str): The name of the configuration JSON file.
-
     Attributes:
-        _config (dict): The loaded configuration data.
-        config (types.MappingProxyType): An immutable view of the configuration data.
-
-    Methods:
-        load_config(): Load and validate the configuration from the JSON file.
-        _config_file_path(file_name): Get the full path to a specific configuration file.
+        _config (types.MappingProxyType): The loaded configuration data.
     """
 
     def __init__(self):
-        # self._config = self._load_config(file_name, path)
         self._config = None
 
     def __getitem__(self, key):
-        # Allow accessing configuration values using subscript notation
+        """
+        Allow accessing configuration values using subscript notation.
+
+        Args:
+            key (str): The configuration key to retrieve the value for.
+
+        Returns:
+            The value associated with the specified key, or None if the key does not exist.
+        """
         return self._config.get(key)
 
 
@@ -47,12 +46,12 @@ class ConfigLoader:
         Returns:
             types.MappingProxyType: The loaded configuration data.
         """
-        return self._load_config(path, path=True)
+        return self._load_config(path, is_path=True)
 
 
     def load_config(self, file_name):
         """
-        Load and validate the configuration from a JSON file using appdirs.
+        Load and validate the configuration from a JSON file.
 
         Args:
             file_name (str): The name of the configuration JSON file.
@@ -60,38 +59,37 @@ class ConfigLoader:
         Returns:
             types.MappingProxyType: The loaded configuration data.
         """
-        return self._load_config(file_name, path=False)
+        return self._load_config(file_name, is_path=False)
 
 
-    def _load_config(self, file_name, path=False):
+    def _load_config(self, file_name, is_path=False):
         """
         Load and validate the configuration from the JSON file.
 
         Args:
             file_name (str): The name or path of the configuration JSON file.
-            path (bool): True if file_name is a path, False if it's a filename.
+            is_path (bool): True if file_name is a path, False if it's a filename.
 
         Returns:
             types.MappingProxyType: The loaded configuration data.
         """
-        config_file = Path(file_name) if path else Ygg.get_path(file_name)
+        config_file = Path(file_name) if is_path else Ygg.get_path(file_name)
 
         if config_file is None:
-            return {}  # Return an empty dictionary or handle the error as needed.
+            return types.MappingProxyType({})  # Return an empty dictionary  # Return an empty dictionary
 
-        config = {}
         try:
             with open(config_file, "r") as f:
                 config = json.load(f)
-                # Perform validation and error checking on the loaded data as needed.
-                # ...
-
+                # TODO: Perform validation and error checking on the loaded data if needed.
+                self._config = types.MappingProxyType(config)
         except json.JSONDecodeError as e:
             raise json.JSONDecodeError(f"Error parsing config file '{config_file}': {e}")
         except TypeError as e:
             raise TypeError(f"Error parsing config file '{config_file}': {e}")
-
-        self._config = types.MappingProxyType(config)
+        except Exception as e:
+            logging.error(f"Unexpected error loading config file '{config_file}': {e}")
+            raise
 
         return self._config
 
