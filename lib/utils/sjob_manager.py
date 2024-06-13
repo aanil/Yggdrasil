202d8d6d0deb5dfd5dcbcba6b629ff0e12684f47
glrs <5999366+glrs@users.noreply.github.com>
2024-06-13 10:37:24 +0200

    Add docstrings | improve logging

--- lib/utils/sjob_manager.py
+++ lib/utils/sjob_manager.py
@@ -118,11 +118,35 @@ logging = custom_logger(__name__.split('.')[-1])
 #################################################################################################
 
 class SlurmJobManager:
+    """
+    Manages the submission and monitoring of Slurm jobs.
+
+    Attributes:
+        polling_interval (float): Interval for polling job status.
+        command_timeout (float): Timeout for Slurm commands.
+    """
     def __init__(self, polling_interval=10.0, command_timeout=8.0):
+        """
+        Initialize the SlurmJobManager with specified polling interval and command timeout.
+
+        Args:
+            polling_interval (float): Interval for polling job status. Defaults to 10.0 seconds.
+            command_timeout (float): Timeout for Slurm commands. Defaults to 8.0 seconds.
+        """
         self.polling_interval = configs.get("job_monitor_poll_interval", polling_interval)
         self.command_timeout = command_timeout
 
+
     async def submit_job(self, script_path):
+        """
+        Submits a Slurm job using the specified script.
+
+        Args:
+            script_path (str): Path to the Slurm script.
+
+        Returns:
+            str: The job ID if submission is successful, None otherwise.
+        """
         sbatch_command = ["sbatch", script_path]
         try:
             result = await asyncio.create_subprocess_exec(
@@ -142,7 +166,6 @@ class SlurmJobManager:
             if job_id:
                 logging.info(f"Job submitted with ID: {job_id}")
                 return job_id
-
         except asyncio.TimeoutError:
             logging.error("Timeout while submitting job.")
         except Exception as e:
@@ -161,17 +184,36 @@ class SlurmJobManager:
     #         await asyncio.sleep(self.polling_interval)
 
     async def monitor_job(self, job_id, sample):
-        """Monitors the specified job and calls the sample's post-process method based on job status."""
+        """
+        Monitors the specified job and delegates status handling to the check_status method.
+
+        This method continuously checks the status of a Slurm job until it completes or fails.
+        Depending on the final status, it calls the check_status method to handle the sample accordingly.
+
+        Args:
+            job_id (str): The job ID.
+            sample (object): The sample object with id attribute.
+        """
         logging.debug(f"[{sample.id}] Job {job_id} submitted for monitoring.")
         while True:
             status = await self._job_status(job_id)
             if status in ["COMPLETED", "FAILED", "CANCELLED", "TIMEOUT"]:
-                logging.info(f"Job {job_id} status: {status}")
+                # logging.info(f"Job {job_id} status: {status}")
                 self.check_status(job_id, status, sample)
                 break
             await asyncio.sleep(self.polling_interval)
 
+
     async def _job_status(self, job_id):
+        """
+        Retrieves the status of a Slurm job.
+
+        Args:
+            job_id (str): The job ID.
+
+        Returns:
+            str: The status of the job.
+        """
         sacct_command = f"sacct -n -X -o State -j {job_id}"
         try:
             process = await asyncio.create_subprocess_shell(
@@ -179,7 +221,6 @@ class SlurmJobManager:
                 stdout=asyncio.subprocess.PIPE,
                 stderr=asyncio.subprocess.PIPE,
             )
-            # stdout, stderr = await asyncio.wait_for(process.communicate(), self.command_timeout)
             stdout, stderr = await process.communicate()
 
             if stderr:
@@ -189,7 +230,6 @@ class SlurmJobManager:
                 stdout_decoded = stdout.decode().strip()
                 logging.debug(f"sacct stdout for job {job_id}: {stdout_decoded}")
                 return stdout_decoded
-
         except asyncio.TimeoutError:
             logging.error(f"Timeout while checking status of job {job_id}.")
         except Exception as e:
@@ -200,18 +240,20 @@ class SlurmJobManager:
     @staticmethod
     def check_status(job_id, status, sample):
         """
-        Checks the status of a job and calls the appropriate method on the sample object.
+        Monitors the specified job and delegates status handling to the check_status method.
+
+        This method continuously checks the status of a Slurm job until it completes or fails.
+        Depending on the final status, it calls the check_status method to handle the sample accordingly.
 
         Args:
             job_id (str): The job ID.
-            status (str): The status of the job.
-            sample (object): The sample object (must have a post_process method and id attribute).
+            sample (object): The sample object with id attribute.
         """
-        print(f"Job {job_id} status: {status}")
+        logging.debug(f"Job {job_id} status: {status}")
         if status == "COMPLETED":
-            print(f"Sample {sample.id} processing completed.")
+            logging.info(f"Sample {sample.id} processing completed.")
             sample.post_process()
             sample.status = "completed"
         elif status in ["FAILED", "CANCELLED", "TIMEOUT"]:
             sample.status = "failed"
-            print(f"Sample {sample.id} processing failed.")
+            logging.info(f"Sample {sample.id} processing failed.")
