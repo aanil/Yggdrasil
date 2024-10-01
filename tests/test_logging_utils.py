4950c0777b508653b8e0ae3fa813b732ab591973
glrs <5999366+glrs@users.noreply.github.com>
2024-10-01 17:31:24 +0200

    Update for renaming lib/utils to lib/core_utils | other fixes

--- tests/test_logging_utils.py
+++ tests/test_logging_utils.py
@@ -1,16 +1,14 @@
 import unittest
 from unittest.mock import patch, MagicMock
-from lib.utils.logging_utils import configure_logging
+from lib.core_utils.logging_utils import configure_logging
 
 class TestConfigureLogging(unittest.TestCase):
     @patch('lib.utils.logging_utils.logging')
-    @patch('lib.utils.logging_utils.appdirs')
     @patch('lib.utils.logging_utils.datetime')
     @patch('lib.utils.logging_utils.Path')
-    def test_configure_logging(self, mock_path, mock_datetime, mock_appdirs, mock_logging):
+    def test_configure_logging(self, mock_path, mock_datetime, mock_logging):
         # Set up the mocks
         mock_path.return_value.mkdir.return_value = None
-        mock_appdirs.user_log_dir.return_value = '/path/to/logs'
         mock_datetime.now.return_value.strftime.return_value = '2022-01-01_00.00.00'
         mock_logging.DEBUG = 10
         mock_logging.INFO = 20
@@ -20,7 +18,7 @@ class TestConfigureLogging(unittest.TestCase):
         configure_logging(debug=True)
 
         # Assert the mocks were called correctly
-        mock_path.assert_called_once_with('/path/to/logs')
+        mock_path.assert_called_once_with('yggdrasil_workspace/logs')
         mock_path.return_value.mkdir.assert_called_once_with(parents=True, exist_ok=True)
         mock_datetime.now.assert_called_once()
         mock_datetime.now.return_value.strftime.assert_called_once_with("%Y-%m-%d_%H.%M.%S")
