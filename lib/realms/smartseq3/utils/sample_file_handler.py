3cb89cb3c3fc21f3c399000b7d92f892d20e8881
glrs <5999366+glrs@users.noreply.github.com>
2024-10-04 11:14:10 +0200

    Apply Black formatting

--- lib/realms/smartseq3/utils/sample_file_handler.py
+++ lib/realms/smartseq3/utils/sample_file_handler.py
@@ -1,13 +1,12 @@
 import glob
 import shutil
-
 from pathlib import Path
 
+from lib.core_utils.logging_utils import custom_logger
 from lib.realms.smartseq3.utils.ss3_utils import SS3Utils
 
-from lib.core_utils.logging_utils import custom_logger
+logging = custom_logger(__name__.split(".")[-1])
 
-logging = custom_logger(__name__.split('.')[-1])
 
 class SampleFileHandler:
     """
@@ -44,31 +43,34 @@ class SampleFileHandler:
         self.sample_id = sample.id
 
         # NOTE: Temporary solution to keep the plate id for the transition period
-        self.plate = sample.id # sample.sample_data.get('customer_name', sample.id)
+        self.plate = sample.id  # sample.sample_data.get('customer_name', sample.id)
 
         self.flowcell_id = sample.flowcell_id
         self.barcode = sample.barcode
-        self.project_id = sample.project_info.get('project_id', None)
-        self.project_name = sample.project_info.get('project_name', None) # TODO: Remove this if not used anywhere else / see todo above
-        self.sample_ref = sample.project_info.get('ref_genome', None)
-        self.organism = sample.project_info.get('organism', None)
+        self.project_id = sample.project_info.get("project_id", None)
+        self.project_name = sample.project_info.get(
+            "project_name", None
+        )  # TODO: Remove this if not used anywhere else / see todo above
+        self.sample_ref = sample.project_info.get("ref_genome", None)
+        self.organism = sample.project_info.get("organism", None)
         self.config = sample.config
 
         # Define sample folder structure
-        self.base_dir = Path(self.config['smartseq3_dir']) / 'projects' / self.project_name
+        self.base_dir = (
+            Path(self.config["smartseq3_dir"]) / "projects" / self.project_name
+        )
         self.sample_dir = self.base_dir / self.sample_id
-        self.zumis_output_dir = self.sample_dir / 'zUMIs_output'
-        self.stats_dir = self.sample_dir / 'zUMIs_output' / 'stats'
-        self.expression_dir = self.sample_dir / 'zUMIs_output' / 'expression'
-        self.fastq_files_dir = self.sample_dir / 'fastq_files'
-        self.plots_dir = self.sample_dir / 'plots'
+        self.zumis_output_dir = self.sample_dir / "zUMIs_output"
+        self.stats_dir = self.sample_dir / "zUMIs_output" / "stats"
+        self.expression_dir = self.sample_dir / "zUMIs_output" / "expression"
+        self.fastq_files_dir = self.sample_dir / "fastq_files"
+        self.plots_dir = self.sample_dir / "plots"
 
         # Initialize fastq files
-        self.fastq_files = {'R1': None, 'R2': None, 'I1': None, 'I2': None}
+        self.fastq_files = {"R1": None, "R2": None, "I1": None, "I2": None}
 
         # Define critical file paths
         self.init_file_paths()
-
 
     def init_file_paths(self):
         """
@@ -77,22 +79,28 @@ class SampleFileHandler:
         # zUMIs output files
         self.gene_counts_fpath = self.stats_dir / f"{self.plate}.genecounts.txt"
         self.reads_per_cell_fpath = self.stats_dir / f"{self.plate}.readspercell.txt"
-        self.umicount_inex_loom_fpath = self.expression_dir / f"{self.plate}.umicount.inex.all.loom"
-        self.bc_umi_stats_fpath = self.zumis_output_dir / f"{self.plate}kept_barcodes_binned.txt.BCUMIstats.txt"
+        self.umicount_inex_loom_fpath = (
+            self.expression_dir / f"{self.plate}.umicount.inex.all.loom"
+        )
+        self.bc_umi_stats_fpath = (
+            self.zumis_output_dir
+            / f"{self.plate}kept_barcodes_binned.txt.BCUMIstats.txt"
+        )
         self.zumis_log_fpath = self.sample_dir / f"{self.plate}.zUMIs_runlog.txt"
         self.features_plot_fpath = self.stats_dir / f"{self.plate}.features.pdf"
 
         # Files needed for processing
         self.slurm_script_path = self.base_dir / f"{self.sample_id}_slurm_script.sh"
-        self.barcode_fpath = Path(self.config['smartseq3_dir']) / "barcodes" / f"{self.barcode}.txt"
-        self.barcode_lookup_fpath = Path(self.config['barcode_lookup_path'])
+        self.barcode_fpath = (
+            Path(self.config["smartseq3_dir"]) / "barcodes" / f"{self.barcode}.txt"
+        )
+        self.barcode_lookup_fpath = Path(self.config["barcode_lookup_path"])
 
         # Report output files
         self.umi_stats_fpath = self.stats_dir / f"{self.plate}.umi_stats.txt"
         self.well_barcodes_fpath = self.stats_dir / f"{self.plate}.well_barcodes.txt"
         # TODO: whether PDF or HTML should be decided by the report generator
         self.report_fpath = self.zumis_output_dir / f"{self.plate}_report.pdf"
-        
 
     def ensure_barcode_file(self):
         """
@@ -102,13 +110,16 @@ class SampleFileHandler:
             bool: True if the barcode file exists or is created successfully, False otherwise.
         """
         if not self.barcode_fpath.exists():
-            logging.info(f"Barcode file for '{self.barcode}' does not exist. Creating...")
+            logging.info(
+                f"Barcode file for '{self.barcode}' does not exist. Creating..."
+            )
             logging.debug(f"In: {self.barcode_fpath}")
-            if not SS3Utils.create_barcode_file(self.barcode, self.barcode_lookup_fpath, self.barcode_fpath):
+            if not SS3Utils.create_barcode_file(
+                self.barcode, self.barcode_lookup_fpath, self.barcode_fpath
+            ):
                 logging.error(f"Failed to create barcode file at {self.barcode_fpath}.")
                 return False
         return True
-
 
     def locate_ref_paths(self):
         """
@@ -122,44 +133,54 @@ class SampleFileHandler:
 
             # Check if sample_ref is provided and valid
             if self.sample_ref and self.sample_ref != "Other (-, -)":
-                species_key = self.sample_ref.split(',')[0].split('(')[1].strip().lower()
+                species_key = (
+                    self.sample_ref.split(",")[0].split("(")[1].strip().lower()
+                )
 
             # If sample_ref is None or indicates an unspecified reference, use self.organism
-            if not species_key or species_key == '-' or species_key == 'other':
+            if not species_key or species_key == "-" or species_key == "other":
                 # If so, use self.organism as the species key, ensuring it's not None
                 if self.organism:
                     species_key = self.organism.strip().lower()
                 else:
-                    logging.warning(f"Organism is None for {self.sample_id}. Handle manually.")
+                    logging.warning(
+                        f"Organism is None for {self.sample_id}. Handle manually."
+                    )
                     return None
 
             # Validate species_key before proceeding
             if not species_key:
-                logging.warning(f"Invalid species_key for {self.sample_id}. Handle manually.")
+                logging.warning(
+                    f"Invalid species_key for {self.sample_id}. Handle manually."
+                )
                 return None
 
             # Attempt to retrieve paths, handle KeyError if species_key is not in config
             try:
-                idx_path = Path(self.config['gen_refs'][species_key]['idx_path'])
-                gtf_path = Path(self.config['gen_refs'][species_key]['gtf_path'])
+                idx_path = Path(self.config["gen_refs"][species_key]["idx_path"])
+                gtf_path = Path(self.config["gen_refs"][species_key]["gtf_path"])
             except KeyError:
-                logging.warning(f"Reference for {species_key} species not found in config. Handle {self.sample_id} manually.")
+                logging.warning(
+                    f"Reference for {species_key} species not found in config. Handle {self.sample_id} manually."
+                )
                 return None
 
             # Check the existence of reference files
             if not idx_path.exists() or not gtf_path.exists():
-                missing_files = '\n\t'.join([str(p) for p in [idx_path, gtf_path] if not p.exists()])
-                logging.warning(f"Missing reference genome files: \n[\n\t{ missing_files }\n]")
+                missing_files = "\n\t".join(
+                    [str(p) for p in [idx_path, gtf_path] if not p.exists()]
+                )
+                logging.warning(
+                    f"Missing reference genome files: \n[\n\t{ missing_files }\n]"
+                )
                 return None
 
-            return {
-                'gen_path': idx_path,
-                'gtf_path': gtf_path
-            }
+            return {"gen_path": idx_path, "gtf_path": gtf_path}
         except (IndexError, AttributeError) as e:
-            logging.error(f"Error parsing sample_ref or accessing organism: {str(e)}. Handle {self.sample_id} manually.")
+            logging.error(
+                f"Error parsing sample_ref or accessing organism: {str(e)}. Handle {self.sample_id} manually."
+            )
             return None
-
 
     def locate_fastq_files(self):
         """
@@ -168,28 +189,36 @@ class SampleFileHandler:
         Returns:
             dict or None: Dictionary of FASTQ file paths if all files are found, None otherwise.
         """
-        pattern = Path(self.config['seq_root_dir'], self.project_id, self.sample_id, '*', self.flowcell_id, f"{self.sample_id}_S*_*_*.f*q.gz")
+        pattern = Path(
+            self.config["seq_root_dir"],
+            self.project_id,
+            self.sample_id,
+            "*",
+            self.flowcell_id,
+            f"{self.sample_id}_S*_*_*.f*q.gz",
+        )
         file_paths = glob.glob(str(pattern))
 
         for file_path in file_paths:
             file = Path(file_path)
-            if file.name.endswith(('.fastq.gz', '.fq.gz')):
-                if '_R1_' in file.stem:
-                    self.fastq_files['R1'] = file
-                elif '_R2_' in file.stem:
-                    self.fastq_files['R2'] = file
-                elif '_I1_' in file.stem:
-                    self.fastq_files['I1'] = file
-                elif '_I2_' in file.stem:
-                    self.fastq_files['I2'] = file
+            if file.name.endswith((".fastq.gz", ".fq.gz")):
+                if "_R1_" in file.stem:
+                    self.fastq_files["R1"] = file
+                elif "_R2_" in file.stem:
+                    self.fastq_files["R2"] = file
+                elif "_I1_" in file.stem:
+                    self.fastq_files["I1"] = file
+                elif "_I2_" in file.stem:
+                    self.fastq_files["I2"] = file
 
         if not all(self.fastq_files.values()):
             missing = [key for key, value in self.fastq_files.items() if value is None]
-            logging.warning(f"Missing FASTQ files for {missing} in {Path(pattern).parent}")
+            logging.warning(
+                f"Missing FASTQ files for {missing} in {Path(pattern).parent}"
+            )
             return None
 
         return self.fastq_files
-
 
     def symlink_fastq_files(self):
         """
@@ -200,31 +229,38 @@ class SampleFileHandler:
         """
         try:
             # Construct the path directly from the known structure
-            fastq_parent_dir = Path(self.config['seq_root_dir'], self.project_id, self.sample_id)
+            fastq_parent_dir = Path(
+                self.config["seq_root_dir"], self.project_id, self.sample_id
+            )
 
             # Create symlink for the fastq_parent_dir
             symlink_target = self.fastq_files_dir / fastq_parent_dir.name
             if not symlink_target.exists():
                 symlink_target.symlink_to(fastq_parent_dir)
-                logging.info(f"Symlink created for directory {fastq_parent_dir} in {self.fastq_files_dir}")
+                logging.info(
+                    f"Symlink created for directory {fastq_parent_dir} in {self.fastq_files_dir}"
+                )
             else:
-                logging.debug(f"Symlink for directory {fastq_parent_dir} already exists.")
+                logging.debug(
+                    f"Symlink for directory {fastq_parent_dir} already exists."
+                )
 
             # Handle .md5 and .lst files
-            for file_extension in ['.md5', '.lst']:
+            for file_extension in [".md5", ".lst"]:
                 source_file = fastq_parent_dir.with_suffix(file_extension)
                 if source_file.exists():
                     shutil.copy(source_file, self.fastq_files_dir)
                     logging.info(f"Copied {source_file} to {self.fastq_files_dir}")
                 else:
-                    logging.warning(f"File {source_file} does not exist and was not copied.")
+                    logging.warning(
+                        f"File {source_file} does not exist and was not copied."
+                    )
 
         except Exception as e:
             logging.error(f"Failed to create symlink and copy files: {e}")
             return False
 
         return True
-
 
     # TODO: Add checks to ensure that the paths exist
     def get_stat_files(self):
@@ -235,12 +271,11 @@ class SampleFileHandler:
             dict: Dictionary containing paths to stat files (gene counts, reads per cell, and barcode UMI stat files).
         """
         stats_files = {
-            'genecounts': self.gene_counts_fpath,
-            'readspercell': self.reads_per_cell_fpath,
-            'bc_umi_stats': self.bc_umi_stats_fpath
+            "genecounts": self.gene_counts_fpath,
+            "readspercell": self.reads_per_cell_fpath,
+            "bc_umi_stats": self.bc_umi_stats_fpath,
         }
         return stats_files
-
 
     # TODO: Add checks to ensure that the paths exist
     def get_counts_loom_file(self):
@@ -250,9 +285,7 @@ class SampleFileHandler:
         Returns:
             dict: Dictionary containing paths to UMI count loom files.
         """
-        loom_file = {
-            'umicount_inex': self.umicount_inex_loom_fpath
-        }
+        loom_file = {"umicount_inex": self.umicount_inex_loom_fpath}
         return loom_file
 
     def create_directories(self):
@@ -263,7 +296,9 @@ class SampleFileHandler:
             self.fastq_files_dir.mkdir(exist_ok=True)
             self.plots_dir.mkdir(exist_ok=True)
         else:
-            logging.error(f"Sample {self.sample_id} directory does not exist (yet?): {self.sample_dir}")
+            logging.error(
+                f"Sample {self.sample_id} directory does not exist (yet?): {self.sample_dir}"
+            )
 
     # def create_fastq_folder(self):
     #     """Create fastq_files folder and manage soft links."""
@@ -293,7 +328,9 @@ class SampleFileHandler:
         """
         if not (self.sample_dir.exists() and self.sample_dir.is_dir()):
             # TODO: In this case it might not make sense to continue, probably skip and report the issue (through Slack?)
-            logging.error(f"Sample {self.sample_id} directory does not exist: {self.sample_dir}")
+            logging.error(
+                f"Sample {self.sample_id} directory does not exist: {self.sample_dir}"
+            )
             return
 
         expected_files = [
@@ -302,15 +339,21 @@ class SampleFileHandler:
             self.umicount_inex_loom_fpath,
             self.bc_umi_stats_fpath,
             self.zumis_log_fpath,
-            self.features_plot_fpath
+            self.features_plot_fpath,
         ]
 
-        missing_files = [file.name for file in expected_files if not file.exists()] # or file.stat().st_size == 0]
+        missing_files = [
+            file.name for file in expected_files if not file.exists()
+        ]  # or file.stat().st_size == 0]
 
         if missing_files:
             missing_files_str = "\n\t".join(missing_files)
-            logging.warning(f"Missing or empty crucial zUMIs output files for sample {self.sample_id} in {self.sample_dir}:\n[\n\t{missing_files_str}\n]")
+            logging.warning(
+                f"Missing or empty crucial zUMIs output files for sample {self.sample_id} in {self.sample_dir}:\n[\n\t{missing_files_str}\n]"
+            )
             return False
         else:
-            logging.info(f"All expected zUMIs output files are present for sample {self.sample_id}.")
+            logging.info(
+                f"All expected zUMIs output files are present for sample {self.sample_id}."
+            )
             return True
