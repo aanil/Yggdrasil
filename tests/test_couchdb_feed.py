bae746a3b184fbf386f8c716bfb8b32a0b11d53a
glrs <5999366+glrs@users.noreply.github.com>
2024-10-04 11:18:44 +0200

    Apply Black formatting and linting

--- tests/test_couchdb_feed.py
+++ tests/test_couchdb_feed.py
@@ -29,19 +29,27 @@
 #     unittest.main()
 
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
+
 from trashcan.couchdb_feed import fetch_data_from_couchdb
+
 
 class TestCouchDBFeed(unittest.TestCase):
 
-    @patch('lib.couchdb_feed.get_db_changes')
-    @patch('lib.couchdb_feed.couch_login')
-    @patch('lib.couchdb_feed.ConfigLoader')
-    async def test_fetch_data_from_couchdb(self, mock_ConfigLoader, mock_couch_login, mock_get_db_changes):
+    @patch("lib.couchdb_feed.get_db_changes")
+    @patch("lib.couchdb_feed.couch_login")
+    @patch("lib.couchdb_feed.ConfigLoader")
+    async def test_fetch_data_from_couchdb(
+        self, mock_ConfigLoader, mock_couch_login, mock_get_db_changes
+    ):
         # Mock the dependencies
-        mock_couch_login.return_value = {'projects': 'mock_db'}
-        mock_get_db_changes.return_value = iter([{'details': {'library_construction_method': 'SmartSeq 3'}, 'seq': 'seq1'}])
-        mock_ConfigLoader.return_value.load_config.return_value = {'SmartSeq 3': {'module': 'module1'}}
+        mock_couch_login.return_value = {"projects": "mock_db"}
+        mock_get_db_changes.return_value = iter(
+            [{"details": {"library_construction_method": "SmartSeq 3"}, "seq": "seq1"}]
+        )
+        mock_ConfigLoader.return_value.load_config.return_value = {
+            "SmartSeq 3": {"module": "module1"}
+        }
 
         # Call the function
         result = await fetch_data_from_couchdb()
@@ -52,9 +60,15 @@ class TestCouchDBFeed(unittest.TestCase):
 
         # Assert the dependencies were called with the correct arguments
         mock_couch_login.assert_called_once()
-        mock_get_db_changes.assert_called_once_with('mock_db', last_processed_seq='72679-g1AAAACheJzLYWBgYMpgTmEQTM4vTc5ISXIwNDLXMwBCwxyQVCJDUv3___-zMpiTGBgak3OBYuxpKWaJBgaG2PTgMSmPBUgyNACp_3ADZ7WADTQwTDYzsDTDpjULAC7GKhU')
+        mock_get_db_changes.assert_called_once_with(
+            "mock_db",
+            last_processed_seq="72679-g1AAAACheJzLYWBgYMpgTmEQTM4vTc5ISXIwNDLXMwBCwxyQVCJDUv3___-zMpiTGBgak3OBYuxpKWaJBgaG2PTgMSmPBUgyNACp_3ADZ7WADTQwTDYzsDTDpjULAC7GKhU",
+        )
         mock_ConfigLoader.assert_called_once()
-        mock_ConfigLoader.return_value.load_config.assert_called_once_with("module_registry.json")
+        mock_ConfigLoader.return_value.load_config.assert_called_once_with(
+            "module_registry.json"
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
--- tests/test_logging_utils.py
+++ tests/test_logging_utils.py
@@ -1,15 +1,17 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock, patch
+
 from lib.core_utils.logging_utils import configure_logging
 
+
 class TestConfigureLogging(unittest.TestCase):
-    @patch('lib.utils.logging_utils.logging')
-    @patch('lib.utils.logging_utils.datetime')
-    @patch('lib.utils.logging_utils.Path')
+    @patch("lib.utils.logging_utils.logging")
+    @patch("lib.utils.logging_utils.datetime")
+    @patch("lib.utils.logging_utils.Path")
     def test_configure_logging(self, mock_path, mock_datetime, mock_logging):
         # Set up the mocks
         mock_path.return_value.mkdir.return_value = None
-        mock_datetime.now.return_value.strftime.return_value = '2022-01-01_00.00.00'
+        mock_datetime.now.return_value.strftime.return_value = "2022-01-01_00.00.00"
         mock_logging.DEBUG = 10
         mock_logging.INFO = 20
         mock_logging.getLogger.return_value = MagicMock()
@@ -18,10 +20,14 @@ class TestConfigureLogging(unittest.TestCase):
         configure_logging(debug=True)
 
         # Assert the mocks were called correctly
-        mock_path.assert_called_once_with('yggdrasil_workspace/logs')
-        mock_path.return_value.mkdir.assert_called_once_with(parents=True, exist_ok=True)
+        mock_path.assert_called_once_with("yggdrasil_workspace/logs")
+        mock_path.return_value.mkdir.assert_called_once_with(
+            parents=True, exist_ok=True
+        )
         mock_datetime.now.assert_called_once()
-        mock_datetime.now.return_value.strftime.assert_called_once_with("%Y-%m-%d_%H.%M.%S")
+        mock_datetime.now.return_value.strftime.assert_called_once_with(
+            "%Y-%m-%d_%H.%M.%S"
+        )
         mock_logging.getLogger.assert_called_once()
         mock_logging.getLogger.return_value.setLevel.assert_called_once_with(10)
         mock_logging.getLogger.return_value.addHandler.assert_called()
@@ -32,5 +38,6 @@ class TestConfigureLogging(unittest.TestCase):
         # Assert the logger's level was set to INFO
         mock_logging.getLogger.return_value.setLevel.assert_called_with(20)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
