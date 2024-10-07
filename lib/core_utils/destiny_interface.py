3e335ca1c0f508e739fa22bdf34fc17d554219f6
glrs <5999366+glrs@users.noreply.github.com>
2024-10-07 14:37:42 +0200

    Moved from utils to core_utils (deprecate)

--- lib/core_utils/destiny_interface.py
+++ lib/core_utils/destiny_interface.py
@@ -1,6 +1,6 @@
-
-from abc import ABC, abstractmethod
 import warnings
+from abc import ABC, abstractmethod
+
 
 class DestinyInterface(ABC):
     """
@@ -8,13 +8,17 @@ class DestinyInterface(ABC):
     It defines the interface for processing documents, where each destiny encapsulates a unique pathway
     or processing logic, akin to the diverse fates woven by the Norns under Yggdrasil.
     """
+
     def __init__(self):
-        warnings.warn("DestinyInterface is deprecated and will be removed in future releases, use RealmTemplate instead.", DeprecationWarning)
+        warnings.warn(
+            "DestinyInterface is deprecated and will be removed in future releases, use RealmTemplate instead.",
+            DeprecationWarning,
+        )
 
     @abstractmethod
     def process(self, doc):
         """
-        Process a document according to the specific destiny (strategy). This method needs to be implemented 
+        Process a document according to the specific destiny (strategy). This method needs to be implemented
         by each concrete destiny class, defining how each document's journey unfolds.
 
         :param doc: The document to be processed.
