6e07e68edc282781862f4dd317a9e7e75a2569a0
glrs <5999366+glrs@users.noreply.github.com>
2024-11-12 09:58:11 +0100

    Functionality to parse Slurm CR output, extract status & report location. Use it in TenXRunSample post_process to transfer report to remote server

--- lib/realms/tenx/utils/sample_file_handler.py
+++ lib/realms/tenx/utils/sample_file_handler.py
@@ -1,5 +1,6 @@
+import re
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from lib.core_utils.logging_utils import custom_logger
 
@@ -39,6 +40,7 @@ class SampleFileHandler:
         self.sample_ref: str = sample.project_info.get("ref_genome", "")
         self.organism: str = sample.project_info.get("organism", "")
         self.config: Dict[str, Any] = sample.config
+        self.pipeline_info: Dict[str, Any] = sample.pipeline_info
 
         # Define sample folder structure
         self.project_dir: Path = sample.project_info.get("project_dir", "")
@@ -57,9 +59,68 @@ class SampleFileHandler:
             self.project_dir / f"{self.sample_id}_slurm_script.sh"
         )
 
-        # Report output files
-        # NOTE: Different pipelines may produce summaries in different locations
-        self.summary_fpath: Path = self.sample_dir / "outs" / "web_summary.html"
+        self.slurm_output_path: Path = self.project_dir / f"{self.sample_id}.out"
+        self.slurm_error_path: Path = self.project_dir / f"{self.sample_id}.err"
+
+        # Report file path / Will be set after parsing the output file
+        self._report_path: Optional[Path] = None
+
+    @property
+    def report_path(self):
+        if self._report_path is None:
+            if not self.extract_report_path():
+                return None
+        return self._report_path
+
+    def check_run_success(self) -> bool:
+        """Check if the CellRanger run completed successfully."""
+
+        if not self.slurm_output_path.exists():
+            logging.error(f"CellRanger output file not found: {self.slurm_output_path}")
+            return False
+
+        with open(self.slurm_output_path) as f:
+            content = f.read()
+
+        if "Pipestance completed successfully!" in content:
+            logging.info(
+                f"CellRanger run completed successfully for sample {self.sample_id}"
+            )
+            return True
+        else:
+            logging.error(
+                f"CellRanger did not complete successfully for sample {self.sample_id}"
+            )
+            return False
+
+    def extract_report_path(self) -> bool:
+        """Extract the report path from the Cell Ranger output file."""
+        if not self.slurm_output_path.exists():
+            logging.error(f"CellRanger output file not found: {self.slurm_output_path}")
+            return False
+
+        with open(self.slurm_output_path) as f:
+            content = f.read()
+
+        report_path = None
+        # Patterns to match different pipelines
+        patterns = [r"Run summary HTML:\s+(\S+)", r"web_summary:\s+(\S+)"]
+
+        for pattern in patterns:
+            match = re.search(pattern, content)
+            if match:
+                report_path = Path(match.group(1))
+                break
+
+        if report_path and report_path.exists():
+            self._report_path = report_path
+            logging.info(f"Report path found: {self.report_path}")
+            return True
+        else:
+            logging.error(
+                f"Report path not found in CellRanger output for sample {self.sample_id}"
+            )
+            return False
 
     def get_libraries_csv_path(self) -> Path:
         return self.project_dir / f"{self.sample_id}_libraries.csv"
