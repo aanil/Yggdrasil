34640482690efb95af05327b532f1fe1dc48dd18
glrs <5999366+glrs@users.noreply.github.com>
2024-11-20 10:45:42 +0100

    Remove YggDB check to find/create projects | Move responsibility to modules

--- ygg-mule.py
+++ ygg-mule.py
@@ -34,53 +34,28 @@ def process_document(doc_id):
 
     project_id = document.get("project_id")
 
-    # Check if the project exists in the yggdrasil database
-    existing_document = ydm.check_project_exists(project_id)
+    # Determine the appropriate module to load
+    module_loc = get_module_location(document)
+    if not module_loc:
+        logging.warning(f"No module found for document with ID {doc_id}.")
+        return
 
-    if existing_document is None:
-        projects_reference = document.get("_id")
-        method = document.get("details", {}).get("library_construction_method")
-
-        # Create a new project in the yggdrasil database
-        ydm.create_project(project_id, projects_reference, method)
-        process_project = True
-    else:
-        # Check the status of the existing project
-        status = existing_document.get("status")
-        if status == "completed":
-            logging.info(
-                f"Project with ID {project_id} is already completed. Skipping processing."
-            )
-            process_project = False
-        else:
-            logging.info(
-                f"Project with ID {project_id} is ongoing and will be processed."
-            )
-            process_project = True
-
-    if process_project:
-        # Determine the appropriate module to load
-        module_loc = get_module_location(document)
-        if not module_loc:
-            logging.warning(f"No module found for document with ID {doc_id}.")
-            return
-
-        # Load and execute the module
-        try:
-            RealmClass = Ygg.load_realm_class(module_loc)
-            if RealmClass:
-                realm = RealmClass(document, ydm)
-                if realm.proceed:
-                    asyncio.run(realm.launch())
-                    logging.info("Processing complete.")
-                else:
-                    logging.info(
-                        f"Skipping processing due to missing required information for project: {project_id}"
-                    )
+    # Load and execute the module
+    try:
+        RealmClass = Ygg.load_realm_class(module_loc)
+        if RealmClass:
+            realm = RealmClass(document, ydm)
+            if realm.proceed:
+                asyncio.run(realm.launch())
+                logging.info("Processing complete.")
             else:
-                logging.warning(f"Failed to load module '{module_loc}'.")
-        except Exception as e:
-            logging.error(f"Error while processing document: {e}", exc_info=True)
+                logging.info(
+                    f"Skipping processing due to missing required information for project: {project_id}"
+                )
+        else:
+            logging.warning(f"Failed to load module '{module_loc}'.")
+    except Exception as e:
+        logging.error(f"Error while processing document: {e}", exc_info=True)
 
 
 # TODO: If the module registry doesn’t change often, consider caching it to avoid reloading it every time
