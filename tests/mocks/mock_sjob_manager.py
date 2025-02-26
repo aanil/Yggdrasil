d24d302601ce568eff8b71e5c06193a4f767f2d9
glrs <5999366+glrs@users.noreply.github.com>
2025-02-26 17:50:50 +0100

    Refactor MockSlurmJobManager to use a list of end states and update job monitoring logic to closer represent reality

--- tests/mocks/mock_sjob_manager.py
+++ tests/mocks/mock_sjob_manager.py
@@ -9,6 +9,16 @@ logging = custom_logger(__name__.split(".")[-1])
 
 
 class MockSlurmJobManager(SlurmJobManager):
+
+    slurm_end_states = [
+        "COMPLETED",
+        "FAILED",
+        "CANCELLED",
+        "CANCELLED+",
+        "TIMEOUT",
+        "OUT_OF_ME+",
+    ]
+
     def __init__(self, polling_interval=1.0, command_timeout=8.0):
         self.polling_interval = polling_interval
         self.command_timeout = command_timeout
@@ -29,18 +39,33 @@ class MockSlurmJobManager(SlurmJobManager):
     #         callback(job_id, "COMPLETED")
 
     async def monitor_job(self, job_id, sample):
-        while self.jobs.get(job_id) != "COMPLETED":
+        logging.info(
+            f"Monitoring job {job_id} with poll interval {self.polling_interval}..."
+        )
+        while True:
+            state = await self._job_status(job_id)
+            if state in self.slurm_end_states:
+                break
             await asyncio.sleep(self.polling_interval)
         # logging.info(f"Job {job_id} status: COMPLETED")
-        self.check_status(job_id, "COMPLETED", sample)
+        # self.check_status(job_id, "COMPLETED", sample)
+        self.check_status_new(job_id, state, sample)
 
     async def _start_job(self, job_id):
-        # Simulate a random wait time between 15 and 35 seconds
-        wait_time = random.uniform(15, 35)
+        # Simulate a random wait time between 5 and 10 seconds
+        wait_time = random.uniform(5, 10)
         await asyncio.sleep(wait_time)
         self.jobs[job_id] = "COMPLETED"
+        logging.info(f"Mock job {job_id} done. Updated to COMPLETED.")
 
-    # ... other methods as needed ...
+    async def _job_status(self, job_id: str) -> str:
+        if job_id not in self.jobs:
+            logging.info(
+                f"Detected unknown job {job_id}. Marking as PROCESSING and scheduling completion."
+            )
+            self.jobs[job_id] = "PROCESSING"
+            asyncio.create_task(self._start_job(job_id))
+        return self.jobs[job_id]
 
     @staticmethod
     def check_status(job_id, status, sample):
@@ -60,6 +85,29 @@ class MockSlurmJobManager(SlurmJobManager):
             sample.post_process()
             # sample.status = "completed"
         elif status in ["FAILED", "CANCELLED", "TIMEOUT", "OUT_OF_ME+"]:
+            sample.status = "processing_failed"
+            logging.info(f"[{sample.id}] Job failed.")
+        else:
+            logging.warning(f"[{sample.id}] Job ended with unexpacted status: {status}")
+            sample.status = "processing_failed"
+
+    @staticmethod
+    def check_status_new(job_id, status, sample):
+        """
+        Checks the status of a job and calls the appropriate method on the sample object.
+
+        Args:
+            job_id (str): The job ID.
+            status (str): The status of the job.
+            sample (object): The sample object (must have a post_process method and id attribute).
+        """
+        logging.info("\n")
+        logging.debug(f"[{sample.id}] Job {job_id} status: {status}")
+        if status == "COMPLETED":
+            logging.info(f"[{sample.id}] Job completed successfully.")
+            sample.status = "processed"
+            # sample.status = "completed"
+        elif status in ["FAILED", "CANCELLED", "CANCELLED+", "TIMEOUT", "OUT_OF_ME+"]:
             sample.status = "processing_failed"
             logging.info(f"[{sample.id}] Job failed.")
         else:
