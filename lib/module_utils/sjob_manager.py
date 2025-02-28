6e723874739435721db8efdb79e1a7d26ab782e4
glrs <5999366+glrs@users.noreply.github.com>
2025-02-28 13:57:56 +0100

    Refactor config loading to use ConfigLoader instead of an import time loaded var configs (removed)

--- lib/module_utils/sjob_manager.py
+++ lib/module_utils/sjob_manager.py
@@ -2,9 +2,10 @@ import asyncio
 import re
 import subprocess
 from pathlib import Path
-from typing import Any, Optional, Union
+from typing import Any, Mapping, Optional, Union
 
-from lib.core_utils.config_loader import configs
+# from lib.core_utils.config_loader import configs
+from lib.core_utils.config_loader import ConfigLoader
 from lib.core_utils.logging_utils import custom_logger
 
 logging = custom_logger(__name__.split(".")[-1])
@@ -31,6 +32,8 @@ class SlurmJobManager:
         "OUT_OF_ME+",
     ]
 
+    configs: Mapping[str, Any] = ConfigLoader().load_config("config.json")
+
     def __init__(
         self, polling_interval: float = 10.0, command_timeout: float = 8.0
     ) -> None:
@@ -42,7 +45,7 @@ class SlurmJobManager:
             command_timeout (float, optional): Timeout for Slurm commands in seconds.
                 Defaults to 8.0 seconds.
         """
-        self.polling_interval: float = configs.get(
+        self.polling_interval: float = self.configs.get(
             "job_monitor_poll_interval", polling_interval
         )
         self.command_timeout: float = command_timeout
